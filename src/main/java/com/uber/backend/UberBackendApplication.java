c5d9458c134a60a812281285d4d1cd7caf684b32
kanishka <kanishkakrishna694@gmail.com>
2026-07-22 21:30:07 +0530

    Reverted architecture to Cascade Routing with Redis Sets Blacklisting

--- src/main/java/com/uber/backend/UberBackendApplication.java
+++ src/main/java/com/uber/backend/UberBackendApplication.java
@@ -1,13 +1,19 @@
 package com.uber.backend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UberBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UberBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UberBackendApplication.class, args);
+    }
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
